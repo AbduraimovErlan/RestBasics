e0c771a912cf6a721faa4c67c4dc760ecc212429
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-16 15:23:09 -0400

    APIView

--- RestBasics/urls.py
+++ RestBasics/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path, include
-from api_basic.views import article_list, article_detail
+# from api_basic.views import article_list, article_detail
+from api_basic.views import ArticleAPIView, ArticleDetail
 
 
 urlpatterns = [
-    path('article/', article_list),
-    path('detail/<int:pk>/', article_detail)
+    path('article/', ArticleAPIView.as_view()),
+    path('detail/<int:id>/', ArticleDetail.as_view())
 ]
+
+
+
+# urlpatterns = [
+#     path('article/', article_list),
+#     path('detail/<int:pk>/', article_detail)
+# ]
