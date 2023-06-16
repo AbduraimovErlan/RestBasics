b469dc8bb4c8e52cb9ab6e28c5fd1ee419d75e62
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-16 12:51:07 -0400

    view Function

--- RestBasics/urls.py
+++ RestBasics/urls.py
@@ -1,7 +1,8 @@
+from django.urls import path, include
+from api_basic.views import article_list, article_detail
 
-from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('article/', article_list),
+    path('detail/<int:pk>/', article_detail)
 ]
